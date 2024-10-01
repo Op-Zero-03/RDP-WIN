10f6e6f5f85f611ef3ea2e191201b10d37dfb6a9
zero <149874292+Op-Zero@users.noreply.github.com>
2024-10-01 20:06:15 +0330

    Update Run_RDS_Setup.bat

--- Run_RDS_Setup.bat
+++ Run_RDS_Setup.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "C:\Users\Administrator\Desktop\RDP_Setup.ps1"
+powershell -ExecutionPolicy Bypass -File "C:\Users\Administrator\Desktop\RDP\RDP_Setup.ps1"
 pause
