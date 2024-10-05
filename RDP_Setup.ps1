3f511bb86c358a096bb862ac568bda23b931c282
zero <149874292+Op-Zero@users.noreply.github.com>
2024-10-05 09:42:37 +0330

    Update RDP_Setup.ps1

--- RDP_Setup.ps1
+++ RDP_Setup.ps1
@@ -1,13 +1,15 @@
 $regPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services'
-
 if (-not (Test-Path $regPath)) {
     New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT' -Name 'Terminal Services' -Force
 }
-
 Set-ItemProperty -Path $regPath -Name 'fSingleSessionPerUser' -Value 0 -Force
 Write-Host "fSingleSessionPerUser set to 0."
 Set-ItemProperty -Path $regPath -Name 'MaxInstanceCount' -Value 900 -Force
 Write-Host "MaxInstanceCount set to 900."
+
 Import-Module ServerManager
 Add-WindowsFeature RDS-RD-Server
 Add-WindowsFeature RDS-Licensing
+
+slmgr /skms kms.digiboy.ir
+slmgr /ato
